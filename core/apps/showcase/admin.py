cde98bd91aadd4efa7877f84fa1aa9ab4729c174
lerooystc <lerooystc@gmail.com>
2024-10-03 00:18:41 +0300

    Added test values via fixtures.

--- core/apps/showcase/admin.py
+++ core/apps/showcase/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from .models import Breed
 from .models import Cat
+from .models import Rating
 
 # Register your models here.
 
 admin.site.register(Cat)
 admin.site.register(Breed)
+admin.site.register(Rating)
